be9c796214644bb85842dea33b6831d58763598a
ruthvik <ruthvik.cr7@gmail.com>
2020-06-14 20:53:12 +0530

    initial commit

---
+++ botFlask.py
@@ -0,0 +1,62 @@
+import logging
+from flask import Flask, request
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram import Bot, Update
+#enable logging
+logging.basicConfig(format = '%(asctime)s - %(name)s - %(message)s', level = logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = "1158559291:AAGDQvAIkLh4mPB290xQV-MBtAfpyUz-Ec8"
+
+#Flask part
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Suck it Jian Yang"
+
+@app.route(f'/{TOKEN}', methods=['GET', 'POST'])
+def webhook():
+    #webhook view which reveives updates from telegram
+    #create update object from json-format request.data
+    update = Update.de_json(request.get_json(), bot)
+    #process update
+    dp.process_update(update)
+    return "ok"
+#Flask part
+
+def start(bot, update):
+    print(update)
+    author = update.message.from_user.first_name
+    reply = "Hi {} I'm uh Jian Yang and my uncle in Beijing is vehry corrupt".format(author)
+    bot.send_message(chat_id = update.message.chat_id, text = reply)
+
+def _help(bot, update):
+    help_txt = "If your name is Erichu Bacheman I won't help you, die pig."
+    bot.send_message(chat_id = update.message.chat_id, text = help_txt)
+
+def echo_text(bot, update):
+    reply = update.message.text
+    bot.send_message(chat_id = update.message.chat_id, text = reply)
+
+def echo_sticker(bot, update):
+    bot.send_sticker(chat_id = update.message.chat_id, sticker = update.message.sticker.file_id)
+
+def error(bot, update):
+    author = update.message.from_user.first_name
+    logger.error("You just uh destroy my rehfrigerator {}.\n Update {} caused error {}".format(author, update, update.error))
+
+if __name__ == "__main__":
+    bot = Bot(TOKEN)
+    bot.set_webhook("https://d3553466bc0b.ngrok.io/" + TOKEN)
+
+    #Dispatcher
+    dp = Dispatcher(bot, None) #None is an update_queue
+    #The dispatcher will have multiple handlers
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("help",_help))
+    dp.add_handler(MessageHandler(Filters.text,echo_text))
+    dp.add_handler(MessageHandler(Filters.sticker, echo_sticker))
+    dp.add_error_handler(error)
+    app.run(port = 8443)
